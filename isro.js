6e4dd1b4688b7fb67c9906711828b67ef2d4c123
Dave Solomon <dave.c.solomon@gmail.com>
2018-12-20 19:07:00 -0500

    Cursed filenames.

---
+++ isro.js
@@ -0,0 +1,70 @@
+const permuter = {
+  initEvents: function() {
+    $("#generatePermutations").click(() => {
+      this.generatePermutations();
+    });
+  },
+  init: function() {
+    this.initEvents();
+  },
+  arrayHasValues: function(arrayToCheck) {
+    return arrayToCheck.some(v => v.length > 0);
+  },
+  generatePermutations: function() {
+    const itemId = $("#itemId").val();
+
+    const slotOneValues = $("#slotOnePerks")
+      .val()
+      .split(" ");
+
+    if (!itemId) {
+      $("#dimWishListContent").val(
+        "You need to enter the item ID, a wildcard, or a category hash ID."
+      );
+      return;
+    }
+
+    if (!this.arrayHasValues(slotOneValues)) {
+      $("#dimWishListContent").val("You need to select at least one perk.");
+      return;
+    }
+
+    const slotTwoValues = $("#slotTwoPerks")
+      .val()
+      .split(" ");
+
+    const slotThreeValues = $("#slotThreePerks")
+      .val()
+      .split(" ");
+
+    const slotFourValues = $("#slotFourPerks")
+      .val()
+      .split(" ");
+
+    const slotFiveValues = $("#slotFivePerks")
+      .val()
+      .split(" ");
+
+    const generatedPermutations = [];
+
+    slotFiveValues.forEach(sv => {
+      slotFourValues.forEach(so => {
+        slotThreeValues.forEach(sh => {
+          slotTwoValues.forEach(sw => {
+            slotOneValues.forEach(sn => {
+              const perkString = [sn, sw, sh, so, sv]
+                .filter(v => v && v !== "")
+                .join(",");
+
+              generatedPermutations.push(
+                `dimwishlist:item=${itemId}&perks=${perkString}`
+              );
+            });
+          });
+        });
+      });
+    });
+
+    $("#dimWishListContent").val(generatedPermutations.join("\n"));
+  }
+};
