7316c0e96b566904f6183c8dd30e3e2da12897ec
Dave Solomon <dave.c.solomon@gmail.com>
2018-12-27 18:58:11 -0500

    DOUBLE IMPACT

--- isro.js
+++ isro.js
@@ -74,6 +74,7 @@ const permuter = {
     "Hitmark IS": 13792278,
     "Honed Edge": 2827049491,
     "Hungry Edge": 436053704,
+    "Impact Casing": 3796465595,
     "IS 2 Classic": 2209280307,
     "Kill Clip": 1015611457,
     "King Dot K2": 1452368633,
